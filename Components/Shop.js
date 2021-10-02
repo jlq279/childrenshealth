2260eddc07f31b0c9a07cd726e1b5c851c1ef666
Arnold-Venter <42255179+SmileCoder1@users.noreply.github.com>
2021-10-02 16:18:51 -0500

    Shop

---
+++ Components/Shop.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { Icon } from "react-native-elements"
+import styled from "styled-components";
+
+const Shop = () => {
+    return (
+        <Text>Some text here</Text>
+    )
+}
+
+export default Shop;
