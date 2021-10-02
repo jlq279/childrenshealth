1793cd5e14daf53ccc99a7a5d574416c9e210cd9
jlq279 <joyce.l.qin@utexas.edu>
2021-10-02 02:05:19 -0500

    weird css

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, Image, Button, Alert, FlatList } from 'react-native';
 
 export default function App() {
+  todoItems = ["Quest 1", "Quest 2"];
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!!!</Text>
+      <Text style={styles.name}>Ben Dover</Text>
+      <Text style={styles.level}>Level: 69</Text>
       <StatusBar style="auto" />
-      <Image
+      <SafeAreaView style={{padding: 16}}>
+          <Text style={styles.quests}>Quests</Text>
+          <FlatList>
+              data={todoItems}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({item, index}) => {
+                  return (
+                      <View style={styles.todo}>
+                          <Text>{item}</Text>
+                      </View>
+                  )
+              }}
+          </FlatList>
+      </SafeAreaView>
+      {/* <Image
         style={styles.stretch}
         source={{
           uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
@@ -16,7 +32,7 @@ export default function App() {
       <Button
         title="Press me"
         onPress={() => Alert.alert('Simple Button pressed')}
-      />
+      /> */}
     </View>
   );
 }
@@ -24,6 +40,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     paddingTop: 50,
+  },
+  name: {
+    position: 'absolute',
+    width: 139,
+    height: 35,
+    left: 201,
+    top: 33,
+
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 30,
+    lineHeight: 35,
+
+    color: '#000000',
+  },
+  level: {
+    position: 'absolute',
+    width: 79,
+    height: 23,
+    left: 201,
+    top: 76,
+
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 20,
+    lineHeight: 23,
+
+    color: '#000000',
+  },
+  quests: {
+    position: 'absolute',
+    width: 76,
+    height: 28,
+    left: 24,
+    top: 320,
+
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 24,
+    lineHeight: 28,
+
+    color: '#000000',
+  },
+  todo: {
+    // position: 'absolute',
+    width: 335,
+    height: 53,
+    // left: 8,
+    // top: 365,
   },
   stretch: {
     width: 50,
